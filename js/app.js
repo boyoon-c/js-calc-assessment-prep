19b33d41ee0c8a518927f8ae1a24825fba5c8fff
ManliestBen <benjamintmanley@gmail.com>
2021-07-06 10:44:04 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Instructions:  You've been given the HTML template for a JavaScript 'calculator.'  
+// Implement functionality such that when two numbers are entered in the input fields, 
+//    pressing one of the 'operator' buttons results in the mathematical calculation for those two numbers.  
+// (Example:  If the boxes contain 3 and 4, pressing the '+' button should put a 7 in the display box.)
+// (If '-' is pressed, -1 should be displayed.  If '*' is pressed, 12 should be displayed, etc...)
+
+// EASY BONUS:  If the result is odd, change it's color to salmon.
+// MEDIUM BONUS:  Use a single event listener to accomplish this task, rather than 4 of them.
